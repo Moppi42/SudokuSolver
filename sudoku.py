08154bbf53d517293fd7eebcf4aacee93a66369d
Moppi42 <76950578+Moppi42@users.noreply.github.com>
2024-01-21 16:18:39 +0100

    Benchmark for EVIL board

--- sudoku.py
+++ sudoku.py
@@ -1,6 +1,6 @@
-import time
 from copy import deepcopy
 from itertools import chain
+import timeit
 
 
 def get_row_items(board: list[int], index: int) -> set[int]:
@@ -46,16 +46,16 @@ def get_possible_items(board: list[int], index: int) -> set[int]:
 
     block_start_row = (row_index // 3) * 3
     block_start_col = (col_index // 3) * 3
-    start_index = block_start_row * 9 + block_start_col
+    block_start_index = block_start_row * 9 + block_start_col
 
     global _ALL_NUMBERS
     return _ALL_NUMBERS - set(
         chain(
             board[row_start:row_start + 9],
             board[col_index::9],
-            board[start_index:start_index + 3],
-            board[start_index + 9:start_index + 12],
-            board[start_index + 18:start_index + 21]
+            board[block_start_index:block_start_index + 3],
+            board[block_start_index + 9:block_start_index + 12],
+            board[block_start_index + 18:block_start_index + 21]
         )
     )
 
@@ -81,7 +81,7 @@ def solve_helper(output_board: list[int], holes: list[int], depth: int) -> bool:
 
 def solve(board: list[int]) -> list[int]:
     output_board = deepcopy(board)
-    holes = get_holes(board)
+    holes = sorted(get_holes(board), key=lambda idx: len(get_possible_items(output_board, idx)))
     solve_helper(output_board, holes, 0)
     return output_board
 
@@ -92,8 +92,7 @@ def print_board(board: list[int]) -> None:
         print(", ".join(str(x) for x in board[start: start + 9]))
 
 
-# for this board the function solve_helper gets called 4433 times
-BOARD: list[int] = [
+SIMPLE_BOARD: list[int] = [
     5, 3, 0, 0, 7, 0, 0, 0, 0,  #
     6, 0, 0, 1, 9, 5, 0, 0, 0,  #
     0, 9, 8, 0, 0, 0, 0, 6, 0,  #
@@ -105,7 +104,29 @@ BOARD: list[int] = [
     0, 0, 0, 0, 8, 0, 0, 7, 9,  #
 ]
 
-import timeit
+EVIL_BOARD: list[int] = [
+    0, 0, 0, 0, 0, 0, 0, 0, 0,  #
+    0, 0, 0, 0, 0, 0, 5, 2, 3,  #
+    0, 0, 0, 0, 0, 0, 0, 1, 8,  #
+    0, 0, 0, 0, 0, 0, 0, 0, 0,  #
+    0, 0, 9, 0, 7, 4, 0, 6, 0,  #
+    0, 0, 4, 6, 1, 0, 0, 0, 7,  #
+    0, 5, 8, 0, 4, 3, 0, 0, 0,  #
+    0, 4, 0, 0, 2, 0, 0, 3, 0,  #
+    0, 6, 7, 0, 8, 1, 0, 9, 4,  #
+]
 
-# takes roughly 82-83 seconds on my machine => so one solve takes on average roughly 8.3ms
-print(timeit.timeit("solve(BOARD)", setup="from __main__ import solve, BOARD", number=10000))
+
+def main():
+    count = 100
+    duration = timeit.timeit("solve(EVIL_BOARD)", setup="from __main__ import solve, EVIL_BOARD", number=count)
+    average = duration * 1000 / count
+    print(f"Evil board: {count} iterations took {duration}s. Average: {average}ms")
+
+    duration = timeit.timeit("solve(SIMPLE_BOARD)", setup="from __main__ import solve, SIMPLE_BOARD", number=count)
+    average = duration * 1000 / count
+    print(f"Simple board: {count} iterations took {duration}s. Average: {average}ms")
+
+
+if __name__ == '__main__':
+    main()
